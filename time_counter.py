7e4b993495010951f5f069dad072cc764cd4ab73
SanAntonik <sanantonik@gmail.com>
2022-02-09 21:20:21 +0200

    added the possibility to append resulting calculations to a file.

--- time_counter.py
+++ time_counter.py
@@ -13,7 +13,6 @@ def time_counter(PATH):
                 sport = int(content[2])
 
             study = content[1].split("_")
-            print(study)
             math += int(study[0])
             computer_science += int(study[1])
             english += int(study[2])
@@ -21,7 +20,7 @@ def time_counter(PATH):
         return [month, math//60, computer_science//60, english//60, sport]
 
 
-def handler(info, show=True, add=False):
+def handler(info, show=True, append_PATH=""):
     month, math, computer_science, english, sport = info
     result = f"""
     {month}:
@@ -29,17 +28,16 @@ def handler(info, show=True, add=False):
         CS: {computer_science} hours.
         English: {english} hours.
         Total study time: {math + computer_science + english} hours.
-        Sport: {sport} times.
-        
-    """
+        Sport: {sport} times.\n"""
 
     if show:
         print(result)
-    if add:
-        # TODO: add string 'result' to file
-        pass
+    if append_PATH:
+        with open(append_PATH, "a") as f:
+            f.write(result)
 
 
 if __name__ == '__main__':
-    PATH = "C:/Users/San/Documents/inf/time monitoring/studying time - Copy.txt"
+    PATH = "C:/Users/San/Documents/inf/time monitoring/studying time.txt"
+    append_PATH = "C:/Users/San/Documents/inf/time monitoring/2022 studies time monitoring.txt"
     handler(time_counter(PATH))

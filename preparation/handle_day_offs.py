d0c05dbf266ada4837943d65601c0332fec5c2b9
SanAntonik <sanantonik@gmail.com>
2023-08-23 16:41:32 +0300

    Add documentation and make handle_day_offs func work again

--- preparation/handle_day_offs.py
+++ preparation/handle_day_offs.py
@@ -1,10 +1,24 @@
-def handle_day_offs(DAY_OFFS):
-    # Create two day_offs vars for generate_report
-    # func. With these two vars, reports will
-    # provide a more detailed picture of the month
-    day_offs_count = len(DAY_OFFS)
+def handle_day_offs(df):
+    """
+    Summary:
+        Calculate and handle days off in the DataFrame.
+        Generates information about the count and specific
+        days of relaxation.
+    Args:
+        df (DataFrame): Input DataFrame containing daily data.
+            Must have a 'DKind' column indicating work or relax days.
+    Returns:
+        day_offs_count (int): Count of relaxation days.
+        day_offs_str (str): String containing the specific days of
+            relaxation, or a message indicating studying every day.
+    """
+    # Find the days on which you relaxed
+    day_offs = df[df["DKind"] == "relax"]["Day"].to_list()
+    # Create two variables for the generate_report function
+    # These variables will provide a more detailed picture of the month
+    day_offs_count = len(day_offs)
     if day_offs_count > 0:
-        day_offs_str = ', '.join(map(str, DAY_OFFS))
+        day_offs_str = ', '.join(map(str, day_offs))
     else:
         day_offs_str = "you studied every day"
     return day_offs_count, day_offs_str
